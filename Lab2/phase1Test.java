56be3943cd75e338604b2248c1f22836de4f965b
Carter Brezinski <Carter.Brezinski@gmail.com>
2019-10-03 13:38:39 -0600

    Initial creation of the phase1Test.java file
    This is where the main method will be held to create, setData, getData, and deleteData from the linked list.

---
+++ Lab2/phase1Test.java
@@ -0,0 +1,21 @@
+public class phase1Test;
+{
+
+/* 
+Processes that need to be achieved in this
+main method:
+- add a node to the linked list
+- retrieve a node from the linked list
+- delete a node from the linked list
+- print list from head -> tail, since this is only phase 1
+*/
+
+	public static void main(String args[])
+	{
+	//The following processes will need to be coded:
+	//Creating an object of the class ListElement
+	//Add an element onto the pre-existing LinkedList named 'ListElement'
+	//Retrieve an element from the pre-existing LinkedList named 'ListElement'
+	//Delete an item from the LinkedList 'ListElement'
+	}
+}
